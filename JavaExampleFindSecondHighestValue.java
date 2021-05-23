802531af68f98cc2dbb35aadff6e4a1d0aab3fbb
Crystas <christus.t@outlook.com>
2021-05-23 22:37:42 +0530

    Grouping by designation

--- JavaExampleFindSecondHighestValue.java
+++ JavaExampleFindSecondHighestValue.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class JavaExampleFindSecondHighestValue {
     public static void main(String args[]) {
@@ -10,17 +14,28 @@ public class JavaExampleFindSecondHighestValue {
         employees.add(new Employee("Jhon", "Manager", 1000.45f));
         employees.add(new Employee("Jack", "Developer", 500.45f));
         employees.add(new Employee("Ron", "Developer", 510.45f));
-        employees.add(new Employee("Cara", "Sr. Developer", 710.45f));
+        employees.add(new Employee("Tim", "Developer", 510.45f));
+        employees.add(new Employee("Cara", "Sr. Developer", 750.45f));
+        employees.add(new Employee("Jackson", "Sr. Developer", 710.45f));
+        JavaExampleFindSecondHighestValue javaExampleFindSecondHighestValue = new JavaExampleFindSecondHighestValue();
 
-        Optional<Employee> secondHighestEmployee = new JavaExampleFindSecondHighestValue().getSecondHighestSalariedEmployee(employees);
-        secondHighestEmployee.ifPresentOrElse(employee -> {
-            System.out.print("Second Highest Salaries Employee: " + employee);
-        }, () -> System.out.print("List doesn't have enough values"));
+        Map<String, List<Employee>> groupedDesignation = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getDesignation));
+        groupedDesignation.entrySet().stream().forEach(javaExampleFindSecondHighestValue::printSecondHigestSalariedEmployeeOfDesignation);
     }
 
-    public Optional<Employee> getSecondHighestSalariedEmployee(List<Employee> employees){
-        return employees.stream()
-        .sorted(Comparator.comparingDouble(Employee::getSalary).reversed()).limit(2).skip(1).findFirst();
+    public void printSecondHigestSalariedEmployeeOfDesignation(Entry<String, List<Employee>> entry) {
+        String designation = entry.getKey();
+        List<Employee> employees = entry.getValue();
+        this.getSecondHighestSalariedEmployee(employees).ifPresentOrElse(employee -> {
+            System.out.println("Second highest Salaried employee for Designation: " + designation + " is " + employee);
+        }, () -> System.out.print("List doesn't have enough values"));
+
+    }
+
+    public Optional<Employee> getSecondHighestSalariedEmployee(List<Employee> employees) {
+        return employees.stream().sorted(Comparator.comparingDouble(Employee::getSalary).reversed()).limit(2).skip(1)
+                .findFirst();
     }
 
     public static class Employee {
