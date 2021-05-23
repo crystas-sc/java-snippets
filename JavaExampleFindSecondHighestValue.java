e5c6261b5f48155466ee35f911a03b892355d7a9
Crystas <christus.t@outlook.com>
2021-05-23 22:42:36 +0530

    refactor imports

--- JavaExampleFindSecondHighestValue.java
+++ JavaExampleFindSecondHighestValue.java
@@ -2,9 +2,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class JavaExampleFindSecondHighestValue {
